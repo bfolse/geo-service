82a271426b2edf0e11efb019a9c2cee61d760ada
Brian Folse <bmfolse@gmail.com>
2016-06-17 17:06:09 -0400

    banner, fix zipCodeRepository

--- src/main/java/com/ttb/service/geo/repositories/ZipCodeRepository.java
+++ src/main/java/com/ttb/service/geo/repositories/ZipCodeRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import com.ttb.service.geo.entities.ZipCode;
 
-public interface ZipCodeRepository extends PagingAndSortingRepository<ZipCode, String>{
+public interface ZipCodeRepository extends PagingAndSortingRepository<ZipCode, Long>{
 
 	List<ZipCode> findByPostalCode(String postalCode);
 }
